e940867ad08d876107717e299b72bf72e2954e37
Aguerquin <alexis.guerquin@capgemini.com>
2024-08-22 18:35:15 +0200

    feat: setting initalSize and adding cardSizeBar component to the card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Card.css';
+import CardSizeBar from './CardSizeBar';
 
 type CardProps = {
-    size: '1x1' | '2x1' | '1x2' | '2x2';
+    initialSize: '1x1' | '2x1' | '1x2' | '2x2';
     logo: string;
     title: string;
     subtitle: string;
     onFollow: () => void;
 };
 
-const Card: React.FC<CardProps> = ({ size, logo, title, subtitle, onFollow }) => {
+const Card: React.FC<CardProps> = ({ initialSize, logo, title, subtitle, onFollow }) => {
+    const [size, setSize] = useState<'1x1' | '2x1' | '1x2' | '2x2'>(initialSize);
+
+    const handleSizeChange = (newSize: '1x1' | '2x1' | '1x2' | '2x2') => {
+        setSize(newSize);
+    };
+
     return (
         <div className={`grid-item size-${size}`}>
             <div className={`card-content ${size}`}>
@@ -28,6 +35,10 @@ const Card: React.FC<CardProps> = ({ size, logo, title, subtitle, onFollow }) =>
                     <div className="extra-2x2"></div>
                 )}
             </div>
+            <div className="card-trash-button">
+                <i className="fas fa-trash-alt"></i>
+            </div>
+            <CardSizeBar onSizeChange={handleSizeChange} currentSize={initialSize} />
         </div>
     );
 };
